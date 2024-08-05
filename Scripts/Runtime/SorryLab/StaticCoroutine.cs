6e43f5449ddcfb8811126e930afc26b323bc6b8b
Yun <mp678922@gmail.com>
2024-08-05 17:30:02 +0800

    editor coroutine

--- Scripts/Runtime/SorryLab/StaticCoroutine.cs
+++ Scripts/Runtime/SorryLab/StaticCoroutine.cs
@@ -17,9 +17,11 @@ namespace SorryLab {
         }
 
         static StaticCoroutine() {
-            GameObject gameObj = new GameObject("[StaticCoroutine]");
-            m_instance = gameObj.AddComponent<StaticCoroutineMono>();
-            Object.DontDestroyOnLoad(gameObj);
+            if (Application.isPlaying) {
+                GameObject gameObj = new GameObject("[StaticCoroutine]");
+                m_instance = gameObj.AddComponent<StaticCoroutineMono>();
+                Object.DontDestroyOnLoad(gameObj);
+            }
         }
 
         static public Coroutine StartCoroutine(IEnumerator method, System.Action onComplete = null) {
@@ -65,30 +67,56 @@ namespace SorryLab {
                 yield return null;
             }
         }
+        /* instance */
+
+        public bool isPlaying { get { return m_isPlaying; } }
+        Coroutine m_coroutine;
+#if UNITY_EDITOR
+        Unity.EditorCoroutines.Editor.EditorCoroutine m_editorCoroutine;
+#endif
+        bool m_isPlaying = false;
+        public System.Action onComplete;
+        public System.Action onKill;
         public StaticCoroutine(IEnumerator coroutine, System.Action onComplete = null) {
             m_instance.StartCoroutine(Async(coroutine));
             this.onComplete = onComplete;
         }
 
-        /* instance */
-
-        public bool isPlaying { get { return m_isPlaying; } }
-        Coroutine m_coroutine;
-        bool m_isPlaying = false;
-        public System.Action onComplete;
-        public System.Action onKill;
-
         IEnumerator Async(IEnumerator coroutine) {
             m_isPlaying = true;
-            m_coroutine = m_instance.StartCoroutine(coroutine);
-            yield return m_coroutine;
+            if (Application.isPlaying) {
+                m_coroutine = m_instance.StartCoroutine(coroutine);
+                yield return m_coroutine;
+            } else {
+#if UNITY_EDITOR
+                m_editorCoroutine = Unity.EditorCoroutines.Editor.EditorCoroutineUtility.StartCoroutine(coroutine, this);
+                yield return m_editorCoroutine;
+#endif
+            }
             m_isPlaying = false;
             onComplete?.Invoke();
+            if (Application.isPlaying) {
+                m_coroutine = null;
+            } else {
+#if UNITY_EDITOR
+                m_editorCoroutine = null;
+#endif
+            }
         }
 
         public void Kill() {
             onKill?.Invoke();
-            m_instance.StopCoroutine(m_coroutine);
+            if (Application.isPlaying) {
+                if (m_coroutine != null) {
+                    m_instance?.StopCoroutine(m_coroutine);
+                    m_coroutine = null;
+                }
+            } else {
+#if UNITY_EDITOR
+                Unity.EditorCoroutines.Editor.EditorCoroutineUtility.StopCoroutine(m_editorCoroutine);
+                m_editorCoroutine = null;
+#endif
+            }
         }
 
         internal class StaticCoroutineMono : MonoBehaviour { void Awake() { } }
