c073a2944efc480c3ab9473fd6e79ce924d666e4
Yun <mp678922@gmail.com>
2025-01-12 17:43:25 +0800

    小更

--- Scripts/Expansion/Unity/ComponentExpansion.cs
+++ Scripts/Expansion/Unity/ComponentExpansion.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 namespace SorryLab.Expansion {
     static public class ComponentExpansion {
         static public RectTransform GetRectTransform(this Component self) { return self.GetComponent<RectTransform>(); }
+        public static T GetOrAddComponent<T>(this GameObject self) where T : Component {
+            return self.GetComponent<T>() ?? self.AddComponent<T>();
+        }
 
     }
 }
