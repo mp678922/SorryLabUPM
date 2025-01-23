12a8b66ceef5b6e604baf11adf8078b7be97b433
Yun <mp678922@gmail.com>
2025-01-23 18:23:17 +0800

    update

--- Scripts/Runtime/Systems/SorryLab/Pool.cs
+++ Scripts/Runtime/Systems/SorryLab/Pool.cs
@@ -3,20 +3,25 @@ using UnityEngine;
 namespace SorryLab {
     public class Pool<T> where T : Component {
         T _object;
+        Transform _returnParent;
         Queue<T> queue = new();
         public Pool(T obj, int defaultInstance = 0) {
             _object = obj;
             for (int i = 0; i < defaultInstance; i++) {
-                T newObj = Component.Instantiate<T>(_object);
+                T newObj = Object.Instantiate<T>(_object);
                 newObj.transform.SetParent(_object.transform.parent);
                 newObj.gameObject.SetActive(false);
                 queue.Enqueue(newObj);
             }
         }
+        public Pool<T> SetReturnParent(Transform folder) {
+            _returnParent = folder;
+            return this;
+        }
         public T Create(Transform parent = null) {
             T obj = null;
             if (obj == null) {
-                obj = queue.Count == 0 ? Component.Instantiate<T>(_object) : queue.Dequeue();
+                obj = queue.Count == 0 ? Object.Instantiate<T>(_object) : queue.Dequeue();
             }
             obj.transform.SetParent(parent == null ? _object.transform.parent : parent);
             if (obj.gameObject.TryGetComponent<RectTransform>(out RectTransform rect)) {
@@ -33,14 +38,18 @@ namespace SorryLab {
         }
         public T Return(T obj) {
             obj.gameObject.SetActive(false);
-            obj.transform.SetParent(_object.transform.parent);
+            obj.transform.SetParent(GetFolder());
             queue.Enqueue(obj);
             return obj;
         }
         public void Release() {
             while (queue.Count > 0) {
-                Component.Destroy(queue.Dequeue());
+                Object.Destroy(queue.Dequeue());
             }
+        }
+        private Transform GetFolder() {
+            if (_returnParent != null) { return _returnParent; }
+            return _object.transform.parent;
         }
     }
 }
