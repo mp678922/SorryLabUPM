39905a9847219e26cc828ab4ff109df38e009ad6
Yun <mp678922@gmail.com>
2025-01-23 15:08:21 +0800

    TryGetChildHierarchyPath

--- Scripts/Expansion/Unity/ComponentExpansion.cs
+++ Scripts/Expansion/Unity/ComponentExpansion.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 namespace SorryLab.Expansion {
     static public class ComponentExpansion {
         static public RectTransform GetRectTransform(this Component self) { return self.GetComponent<RectTransform>(); }
-        public static T GetOrAddComponent<T>(this GameObject self) where T : Component {
-            return self.GetComponent<T>() ?? self.AddComponent<T>();
+        public static T GetOrAddComponent<T>(this Component self) where T : Component {
+            return self.GetComponent<T>() ?? self.gameObject.AddComponent<T>();
+        }
+        public static bool TryGetChildHierarchyPath(this Component self, Component child, out string path) {
+            Transform current = child.transform;
+            Transform root = self.transform;
+            path = current.name;
+            while (current.parent != null) {
+                if (current.parent == root) { return true; }
+                current = current.parent;
+                path = $"{current.name}/{path}";
+            }
+            return false;
         }
 
     }
