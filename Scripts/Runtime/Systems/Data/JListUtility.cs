ecfb51507600c5f2e467aa1b36f81f15227c6b9e
Yun <mp678922@gmail.com>
2025-02-10 22:51:45 +0800

    fixed

--- Scripts/Runtime/Systems/Data/JListUtility.cs
+++ Scripts/Runtime/Systems/Data/JListUtility.cs
@@ -1,7 +1,7 @@
-// using System;
+using System;
 
-// namespace SorryLab {
-//     public class JListUtility {
-//         static public Func<string, Type> onGetType;
-//     }
-// }
+namespace SorryLab {
+    public class JListUtility {
+        static public Func<string, Type> onGetType;
+    }
+}
