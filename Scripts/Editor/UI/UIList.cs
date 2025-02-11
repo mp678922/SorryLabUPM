a027dbecbd2bae777885304175cbeef04a52720e
Yun <mp678922@gmail.com>
2025-02-11 11:11:05 +0800

    UIlist

--- Scripts/Editor/UI/UIList.cs
+++ Scripts/Editor/UI/UIList.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 using UnityEditor;
 namespace SorryLab.Editor.UI {
     public class UIList<T> : UIElement {
-        CustomMenu _menu = new CustomMenu();
         protected Func<T, T> _onDrawElement;
         private List<T> _list;
         private bool _foldout;
         private Action<bool> _onFoldoutChanged;
         private Color _frameColor = Color.white;
+        private Action _drawOtherContent;
         public UIList(string label, List<T> list, bool foldout, Action<bool> onFoldoutChanged, Func<T, T> onDrawElement) {
             _label = label;
             _list = list;
@@ -24,10 +24,12 @@ namespace SorryLab.Editor.UI {
                     for (int i = 0; i < _list.Count; i++) {
                         _list[i] = _onDrawElement.Invoke(_list[i]);
                     }
+                    _drawOtherContent?.Invoke();
                 }).Draw();
             }).SetColor(_frameColor).Draw();
         }
         public void SetFrameColor(Color color) { _frameColor = color; }
+        public void DrawOtherContent(Action content) { _drawOtherContent = content; }
     }
 }
 #endif
