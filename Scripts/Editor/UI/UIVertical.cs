0c38546334c4835c3bac2452f44ae402111d3cf5
Yun <mp678922@gmail.com>
2025-02-12 02:43:41 +0800

    update

--- Scripts/Editor/UI/UIVertical.cs
+++ Scripts/Editor/UI/UIVertical.cs
@@ -7,19 +7,34 @@ namespace SorryLab.Editor.UI {
     public class UIVertical : UIElement {
         Action onDraw;
         int width;
+        static GUIStyle _outlineStyle;
+        static GUIStyle _backgroundStyle;
         public UIVertical(Action onDraw, int width = 0) {
             if (width > 0) { SetHeight(width); }
             this.width = width;
             this.onDraw = onDraw;
         }
         protected override void OnDraw() {
+            if (_outlineStyle == null || _backgroundStyle == null) {
+                Texture2D borderTexture = new Texture2D(1, 1);
+                borderTexture.SetPixel(0, 0, GUI.color = EditorGUIUtility.isProSkin ? new Color(0.12f, 0.12f, 0.12f) : new Color(0.66f, 0.66f, 0.66f));
+                borderTexture.Apply();
+                _outlineStyle = new GUIStyle(GUI.skin.box);
+                _outlineStyle.normal.background = borderTexture;
+                borderTexture = new Texture2D(1, 1);
+                borderTexture.SetPixel(0, 0, GUI.color = EditorGUIUtility.isProSkin ? new Color(0.22f, 0.22f, 0.22f) : new Color(0.76f, 0.76f, 0.76f));
+                borderTexture.Apply();
+                _backgroundStyle = new GUIStyle(GUI.skin.box);
+                _backgroundStyle.normal.background = borderTexture;
+                _outlineStyle.padding = new RectOffset(-10, -10, -10, -10);
+            }
             _isBeginVertical = true;
             _verticalWidth = width;
-            GUIStyle noBackgroundBox = new GUIStyle(GUI.skin.box);
-            noBackgroundBox.normal.background = null; // 移除背景
-            noBackgroundBox.border = new RectOffset(4, 4, 4, 4);
-            GUILayout.BeginVertical(noBackgroundBox);
+
+            GUILayout.BeginVertical(_outlineStyle);
+            GUILayout.BeginVertical(_backgroundStyle);
             onDraw?.Invoke();
+            GUILayout.EndVertical();
             GUILayout.EndVertical();
             _isBeginVertical = false;
         }
