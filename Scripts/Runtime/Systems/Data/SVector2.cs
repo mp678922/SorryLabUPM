2404d1e699dc17cfd3e59acd8d8baa8036c92749
Yun <mp678922@gmail.com>
2025-02-17 18:52:37 +0800

    fix

--- Scripts/Runtime/Systems/Data/SVector2.cs
+++ Scripts/Runtime/Systems/Data/SVector2.cs
@@ -5,12 +5,21 @@ namespace SorryLab {
     public struct SVector2 {
         public float x;
         public float y;
+
         public static implicit operator Vector2(SVector2 obj) {
             return obj.vector2;
         }
+        public static implicit operator SVector2(Vector2 obj) {
+            return new SVector2 { x = obj.x, y = obj.y };
+        }
+
         public static implicit operator Vector3(SVector2 obj) {
             return new Vector3(obj.x, obj.y, 0f);
         }
+        public static implicit operator SVector2(Vector3 obj) {
+            return new SVector2 { x = obj.x, y = obj.y };
+        }
+
         [JsonIgnore]
         public Vector2 vector2 => new Vector2(x, y);
     }
--- Scripts/Runtime/Systems/Data/SVector3.cs
+++ Scripts/Runtime/Systems/Data/SVector3.cs
@@ -6,12 +6,22 @@ namespace SorryLab {
         public float x;
         public float y;
         public float z;
+
         public static implicit operator Vector2(SVector3 obj) {
             return new Vector2(obj.x, obj.y);
         }
-        public static implicit operator Vector3(SVector3 obj) {
-            return obj.vector3;
+        public static implicit operator SVector3(Vector2 obj) {
+            return new Vector3 { x = obj.x, y = obj.y, z = 0f };
         }
+
+
+        public static implicit operator Vector3(SVector3 obj) {
+            return new Vector3(obj.x, obj.y, 0f);
+        }
+        public static implicit operator SVector3(Vector3 obj) {
+            return new SVector3 { x = obj.x, y = obj.y, z = obj.z };
+        }
+
         [JsonIgnore]
         public Vector3 vector3 => new Vector3(x, y, z);
     }
